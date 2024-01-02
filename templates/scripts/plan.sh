7ee49b48120e82ff8672bdb3f9e85bf5a3fca39a
yuka426 <kotakota.426989@gmail.com>
2024-01-02 22:21:04 +0900

    fix check.sh error

--- templates/scripts/plan.sh
+++ templates/scripts/plan.sh
@@ -3,7 +3,7 @@
 terraform plan -out=tfapply > plan_output.txt
 
 if grep -q "no changes" plan_output.txt; then
-    echo "No changes detected. Exiting the build process."
+    echo "### NOTIFICATION: No changes detected. Exiting the build process."
     exit 1
 else
     echo "Changes detected. Continuing the build process."
