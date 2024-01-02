2082310d5b7eaad614b45b74dbd33b75cdf886b7
yuka426 <kotakota.426989@gmail.com>
2024-01-02 21:59:32 +0900

    add plan.sh

---
+++ templates/scripts/plan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+terraform plan -out=tfapply > plan_output.txt
+
+if grep -q "no changes" plan_output.txt; then
+    echo "No changes detected. Exiting the build process."
+    exit 0
+else
+    echo "Changes detected. Continuing the build process."
+fi
+
+# 一時ファイルの削除
+rm plan_output.txt
