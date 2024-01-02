67f1d15635fb4eb6f369fb1722727df9e3147c5d
yuka426 <kotakota.426989@gmail.com>
2024-01-02 21:43:54 +0900

    fix check.sh

--- templates/scripts/check.sh
+++ templates/scripts/check.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 # Accept Command Line Arguments
-SKIPVALIDATIONFAILURE=$1
 tfValidate=$2
 tfFormat=$3
-tfCheckov=$4
-tfTfsec=$5
 # -----------------------------
 
 echo "### VALIDATION Overview ###"
 echo "-------------------------"
-echo "Skip Validation Errors on Failure : ${SKIPVALIDATIONFAILURE}"
 echo "Terraform Validate : ${tfValidate}"
 echo "Terraform Format   : ${tfFormat}"
 echo "------------------------"
-terraform init
 if (( ${tfValidate} == "Y"))
 then
     echo "## VALIDATION : Validating Terraform code ..."
@@ -35,11 +30,8 @@ echo "Terraform Validate : ${tfValidateOutput}"
 echo "Terraform Format   : ${tfFormatOutput}"
 echo "------------------------"
 
-if (( ${SKIPVALIDATIONFAILURE} == "Y" ))
-then
-  #if SKIPVALIDATIONFAILURE is set as Y, then validation failures are skipped during execution
-  echo "## VALIDATION : Skipping validation failure checks..."
-elif (( $tfValidateOutput == 0 && $tfFormatOutput == 0))
+
+if (( $tfValidateOutput == 0 && $tfFormatOutput == 0))
 then
   echo "## VALIDATION : Checks Passed!!!"
 else
