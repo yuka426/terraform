e040c0249893e36ff529e4728c63a5cc405d45e0
yuka426 <kotakota.426989@gmail.com>
2024-01-19 20:49:19 +0900

    fix v13

--- modules/cicd/s3_artifacts.tf
+++ modules/cicd/s3_artifacts.tf
@@ -56,3 +56,9 @@ resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
   bucket = aws_s3_bucket.codepipeline_bucket.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "xxx" {
+  bucket = "xxx"
+
+
+}
